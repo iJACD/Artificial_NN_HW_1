75cec955b79dfcbf6d0454afb14890aa03cd51c0
iBlasian <jcdobson@valdosta.edu>
2017-09-25 21:29:10 -0400

    started building Neuron class

--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,10 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][][] data = Neuron.andData;
+		double[] weights = Neuron.INITIAL_WEIGHTS;
+		Driver driver = new Driver();
+		Neuron neuron = new Neuron();
 	}
 
 }
--- src/Neuron.java
+++ src/Neuron.java
@@ -4,8 +4,19 @@ public class Neuron {
 											 {{0,1},{0}},
 											 {{1,0},{0}},
 											 {{1,1},{1}}};
-	public static final double LEARNING_RATE = 0.5;
-	String test = "Test Git";
-	//test Git
-	//one more test
+	public static final double LEARNING_RATE = 0.05;
+	public static final double[] INITIAL_WEIGHTS = {Math.random(), Math.random()};
+	
+	public double calcWeightedSum(int[] data, double[] weights){
+		double weightedSum = 0;
+		return weightedSum;
+	}
+	
+	public int applyActivationFunc(double sigmoid){
+		return 0;
+	}
+	
+	public double[] adjustWeight(int[] data, double weights, double error){
+		return null;
+	}
 }
