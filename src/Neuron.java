5e9547452a960f65482e9988de804ca764bfe727
iBlasian <jcdobson@valdosta.edu>
2017-09-25 20:54:44 -0400

    First Commit

---
+++ src/Neuron.java
@@ -0,0 +1,9 @@
+
+public class Neuron {
+	public static final int[][][] andData = {{{0,0},{0}},
+											 {{0,1},{0}},
+											 {{1,0},{0}},
+											 {{1,1},{1}}};
+	public static final double LEARNING_RATE = 0.5;
+	}
+}
